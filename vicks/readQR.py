ae0f4a4ca7c2109f74f2471e005e0a097670dbea
imvickykumar999 <imvickykumar999@gmail.com>
2023-04-13 21:47:56 +0530

    Update readQR.py

--- vicks/readQR.py
+++ vicks/readQR.py
@@ -5,6 +5,9 @@ from pyzbar.pyzbar import decode
 from PIL import Image
 
 file = "input/1.jpg"
+# Extract each frame of any video
+# https://github.com/imvickykumar999/Video-Colorization/blob/b377fcacc84d39ab9ea151b2642866c0ea69496b/video_colorization.py#L30
+
 out = decode(Image.open(file))
 
 out = out[0].data.decode('utf-8')
